ed72e11492f24833966c85cc222f019f07313026
Kohichi Horii <kohichihorii@Kohichis-MacBook-Pro.local>
2019-03-22 14:31:05 -0700

    Add two promises for reject and also resolve

--- src/business-logo.js
+++ src/business-logo.js
@@ -1,22 +1,19 @@
 export class FindDoctor {
 
-  doctorInfo(request) {
-    let request = new XMLHttpRequest();
-    const url = `https://api.betterdoctor.com/2016-03-01/doctors?location=${location}45.5122%2C-122.6587%2C100user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`;
+  doctorInfo(issue) {
+    return new Promise(function(resolve, reject) {}
+      let request = new XMLHttpRequest();
+      const url = `https://api.betterdoctor.com/2016-03-01/doctors?location=${location}45.5122%2C-122.6587%2C100user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`;
 
-    request.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        console.log(typeof response);
-        getElements(response);
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
       }
-    }
-
-    request.open("GET", url, true);
-    request.send();
-    // 
-    // const getElements = function(response) {
-    //   $('.area').text(`Here is a doctor near you ${response.meta.humidity}%`);
-    // }
+      request.open("GET", url, true);
+      request.send();
+    });
   }
 }
--- src/doctor-interface.js
+++ src/doctor-interface.js
@@ -6,10 +6,16 @@ import { FindDoctor } from './business.js';
 
 
 $(document).ready(function() {
-  $('#doctorLocation').submite(function() {
+  $('#doctorLocation').submit(function() {
     event.preventDefault();
     const issueInput = $('#issue').val();
     $('#issue').val("");
 
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      $('.area').text('Here are doctors in ${city} is ${body.main.humidity}%');
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+    });
   });
 });
