907ecfa2fa7ba391153ffd9aa48e312e49691b34
Kohichi Horii <kohichihorii@Kohichis-MacBook-Pro.local>
2019-03-22 08:07:01 -0700

    Add init

---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+module.exports= {
+  entry: './src/weather-interface.js',
+  output: {
+    filename:'bundle.js',
+    path: path.resolve(__dirname,'dist')
+  },
+  devtool: 'eval-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new UglifyJsPlugin({ sourceMap: true }),
+    new CleanWebpackPlugin(['dist']),
+    new Dotenv(),
+    new HtmlWebpackPlugin({
+      title: 'Journal',
+      template: './src/weather.html',
+      inject: 'body'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test:/\.html$/,
+        use: [
+          'html-loader'
+        ]
+      },
+      {
+        test: /\.(gif|png|jpe?g)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/images/'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: [
+          /node_modules/,
+          /spec/
+        ],
+        loader: "eslint-loader"
+      },
+      {
+        test: /\.js$/,
+        exclude: [
+          /node_modules/,
+          /spec/
+        ],
+        loader: "babel-loader",
+        options: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  }
+};
